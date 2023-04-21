d330b5eb8093b59472386b2d90ded4ed90cb0ec5
Andreef76 <andreef76@gmail.com>
2023-04-21 12:05:37 +0500

    Initial commit

---
+++ app/src/main/java/ru/netology/test_123/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package ru.netology.test_123.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/ru/netology/test_123/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ru.netology.test_123.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
